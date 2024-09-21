0abb6816ec96772ab299015f0c3e5b48170e842f
Kleber Vales <ksvales@outlook.pt>
2024-09-21 09:33:29 -0300

    Enviando classe principal

---
+++ src/bigInteger/Test.java
@@ -0,0 +1,20 @@
+package bigInteger;
+
+import java.io.*; 
+import java.util.*; 
+import java.text.*; 
+import java.math.*; 
+import java.util.regex.*;
+
+public class Test {
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    BigInteger bigInt1 = sc.nextBigInteger();
+    BigInteger bigInt2 = sc.nextBigInteger();
+    BigInteger sum = bigInt1.add(bigInt2);
+    BigInteger product = bigInt1.multiply(bigInt2);
+    System.out.println(sum);
+    System.out.println(product);
+}
+}
